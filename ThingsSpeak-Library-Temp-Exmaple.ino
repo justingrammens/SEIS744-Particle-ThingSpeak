0f22451a1b6de0dfe95017d4ee68fad0532db266
Justin Grammens <justin@lab651.com>
2023-11-09 18:57:21 -0600

    k

--- ThingsSpeak-Library-Temp-Exmaple.ino
+++ ThingsSpeak-Library-Temp-Exmaple.ino
@@ -1,12 +1,8 @@
-// This #include statement was automatically added by the Particle IDE.
 #include <Grove_Temperature_And_Humidity_Sensor.h>
 
 DHT dht(D2);
 
-// This #include statement was automatically added by the Particle IDE.
 #include "ThingSpeak.h"
-
-TCPClient client;
 
 unsigned long myChannelNumber = XXXX;    // change this to your channel number
 const char * myWriteAPIKey = "XXXX"; // change this to your channels write API key
@@ -19,20 +15,19 @@ void setup() {
   Serial.begin(9600);
   dht.begin();
 
-
 }
 
 void loop() {
-// read the input on analog pin 0:
-//int sensorValue = analogRead(A0);
 
-temp = dht.getTempFarenheit();
-humidity = dht.getHumidity();
+temp = String(random(60, 80));
+// LETS JUST HAVE A RANDOM TEMP FOR NOW.
+//temp = dht.getTempFarenheit();
 
-Serial.printlnf("Temp: %f", temp);
-Serial.printlnf("Humidity: %f", humidity);
+Serial.printlnf("Temp: ", temp);
+//Serial.printlnf("Humidity: %f", humidity);
 
 // Write to ThingSpeak, field 1, immediately
 ThingSpeak.writeField(myChannelNumber, 1, temp, myWriteAPIKey);
 delay(20000); // ThingSpeak will only accept updates every 15 seconds.
+
 }
